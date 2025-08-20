a8bc06192a34a86191502fbb32ac381ffa07ce6b
FatiaGlacier <dmfatianov@gmail.com>
2025-08-20 22:55:09 +0300

    project initialized

---
+++ src/test/java/com/wms/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wms.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
